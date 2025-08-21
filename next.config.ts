7b600293cfbcaeacc3ee5285ba175c23da02d197
pawafulu_7 <pawafulu_7@yahoo.co.jp>
2025-08-21 13:33:36 +0900

    fix: next/image外部画像ホストの設定追加
    - Speaker Deck、Dev.to、Qiita等の外部画像ホストを許可リストに追加
    - remotePatterns設定で各サービスのCDNを許可
    - Runtime Errorの解決

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,63 @@ const nextConfig: NextConfig = {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'files.speakerdeck.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'speakerdeck.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.dev.to',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'qiita-user-contents.imgix.net',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'zenn.dev',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'b.hatena.ne.jp',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.b.hatena.ne.jp',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
